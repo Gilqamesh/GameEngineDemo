22cffbad74d49e684dd87689c9f6aa831cdb0f22
Erchegyi David <edavid@2-A-1.42heilbronn.de>
2022-04-16 16:18:45 +0200

    add VertexBuffer

--- headers/Core/VertexBuffer.hpp
+++ headers/Core/VertexBuffer.hpp
@@ -1,9 +1,31 @@
 #ifndef VERTEXBUFFER_HPP
 # define VERTEXBUFFER_HPP
 
+# include "pch.hpp"
+
+namespace NAMESPACE
+{
+
 class VertexBuffer
 {
+GLuint GL_ID;
 public:
+    /*
+     * Static version
+     * Cannot be modified
+     */
+    VertexBuffer(const void *data, GLuint size);
+    /*
+     * Dynamic version
+     * Initialize it later and possibly multiple times
+     */
+    VertexBuffer(GLuint size);
+    ~VertexBuffer();
+
+    void bind();
+    void unbind();
 };
+
+}
 
 #endif
--- sources/Core/VertexBuffer.cpp
+++ sources/Core/VertexBuffer.cpp
@@ -1,3 +1,42 @@
 #include "Core/VertexBuffer.hpp"
+#include "Debug/Trace.hpp"
+
+namespace NAMESPACE
+{
+
+VertexBuffer::VertexBuffer(const void *data, GLuint size)
+{
+    TRACE();
+    GLCall(glGenBuffers(1, &GL_ID));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, GL_ID));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
+}
+
+VertexBuffer::VertexBuffer(GLuint size)
+{
+    TRACE();
+    GLCall(glGenBuffers(1, &GL_ID));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, GL_ID));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, size, nullptr, GL_DYNAMIC_DRAW));
+}
+
+VertexBuffer::~VertexBuffer()
+{
+    TRACE();
+    GLCall(glDeleteBuffers(1, &GL_ID));
+}
+
+void VertexBuffer::bind()
+{
+    TRACE();
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, GL_ID));
+}
+
+void VertexBuffer::unbind()
+{
+    TRACE();
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+}
 
 
+}
