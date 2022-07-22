357299102012371a40b5e8015ff633392b013a6d
Érchegyi Dávid <celicaw8@gmail.com>
2022-07-22 11:12:30 +0200

    test Grid

--- test.cpp
+++ test.cpp
@@ -5,18 +5,58 @@
 #include <vector>
 #include <unordered_map>
 #include <cmath>
+#include <array>
 #define NODE_CAPACITY 20
 
 using namespace std;
 
 #define LOG(x) (cout << x << endl)
+#define ArrayCount(x) ((sizeof(x))/sizeof((x)[0]))
+
+uint64_t pow2(int power)
+{
+    static bool called;
+    static uint64_t results[64];
+
+    if (called == false)
+    {
+        called = true;
+        uint64_t result = 1;
+        for (int i = 0; i < ArrayCount(results); ++i)
+        {
+            results[i] = result;
+            result *= 2;
+        }
+    }
+
+    return (results[power]);
+}
+
+uint64_t pow4(int power)
+{
+    static bool called;
+    static uint64_t results[32];
+
+    if (called == false)
+    {
+        called = true;
+        uint64_t result = 1;
+        for (int i = 0; i < ArrayCount(results); ++i)
+        {
+            results[i] = result;
+            result *= 4;
+        }
+    }
+
+    return (results[power]);
+}
 
 struct Rectangle
 {
-    float topLeftX;
-    float topLeftY;
-    float width;
-    float height;
+    int topLeftX;
+    int topLeftY;
+    int width;
+    int height;
 
     inline bool doesIntersect(const Rectangle& other) const
     {
@@ -103,8 +143,8 @@ public:
 private:
     void subdivide(const vector<Rectangle>& rectangles)
     {
-        float halfWidth = boundary.width / 2.0f;
-        float halfHeight = boundary.height / 2.0f;
+        int halfWidth = boundary.width / 2;
+        int halfHeight = boundary.height / 2;
         northWest = new QuadTreeNode({boundary.topLeftX, boundary.topLeftY, halfWidth, halfHeight});
         northEast = new QuadTreeNode({boundary.topLeftX + halfWidth, boundary.topLeftY, halfWidth, halfHeight});
         southWest = new QuadTreeNode({boundary.topLeftX, boundary.topLeftY + halfHeight, halfWidth, halfHeight});
@@ -168,6 +208,20 @@ public:
     }
 };
 
+// class ImprovedQuadTree
+// {
+//     vector<Rectangle> rectangles;
+//     Rectangle _bound;
+//     ImprovedQuadTreeNode _root;
+// public:
+//     ImprovedQuadTree(const Rectangle& bound)
+//         : _bound(bound),
+//         _root(bound)
+//     {}
+
+//     void 
+// };
+
 class ImprovedQuadTree
 {
     vector<Rectangle> rectangles;
@@ -177,7 +231,7 @@ class ImprovedQuadTree
     // todo:
     // change level type to char for smaller bucket size
     // check bucket size for regions
-    unordered_map<unsigned int, unordered_map<unsigned long int, vector<unsigned int>>> levelsToRegions; // Level -> RegionIndex -> vector of Rectangle indices
+    unordered_map<uint8_t, unordered_map<uint64_t, vector<unsigned int>>> levelsToRegions; // Level -> RegionIndex -> vector of Rectangle indices
 public:
     ImprovedQuadTree(const Rectangle& bound)
         : _bound(bound),
@@ -219,7 +273,7 @@ public:
     }
 
 private:
-    bool insert(unsigned int rectangleIndex, unsigned int level, unsigned long int regionIndex)
+    bool insert(unsigned int rectangleIndex, uint8_t level, unsigned long int regionIndex)
     {
         if (level > 31)
         {
@@ -227,11 +281,21 @@ private:
             exit(1);
         }
 
-        int width = (int)_bound.width / (int)pow(2, level);
-        int height = (int)_bound.height / (int)pow(2, level);
-        int topLeftY = regionIndex / (unsigned long)pow(2, level) * height;
-        int topLeftX = (regionIndex % (unsigned long)pow(2, level)) * width;
-        Rectangle curBound = {(float)topLeftX, (float)topLeftY, (float)width, (float)height};
+        int width = (int)_bound.width / pow2(level);
+        int height = (int)_bound.height / pow2(level);
+        int topLeftX = 0;
+        int topLeftY = 0;
+        for (unsigned long startBit = pow4(level - 1); startBit > 0; startBit >>= 2)
+        {
+            topLeftX <<= 1;
+            topLeftY <<= 1;
+            topLeftX += ((regionIndex & startBit) > 0 ? 1 : 0);
+            topLeftY += ((regionIndex & (startBit << 1)) > 0 ? 1 : 0);
+        }
+        topLeftX *= width;
+        topLeftY *= height;
+
+        Rectangle curBound = {topLeftX, topLeftY, width, height};
 
         if (curBound.doesIntersect(rectangles[rectangleIndex]) == false)
             return (false);
@@ -249,45 +313,140 @@ private:
             auto& currentRectangles = levelsToRegions[level][regionIndex];
             for (unsigned int recIndex : currentRectangles)
             {
-                insert(recIndex, level + 1, 4 * regionIndex);
-                insert(recIndex, level + 1, 4 * regionIndex + 1);
-                insert(recIndex, level + 1, 4 * regionIndex + 2);
-                insert(recIndex, level + 1, 4 * regionIndex + 3);
+                insert(recIndex, level + 1, (regionIndex << 2));
+                insert(recIndex, level + 1, (regionIndex << 2) + 1);
+                insert(recIndex, level + 1, (regionIndex << 2) + 2);
+                insert(recIndex, level + 1, (regionIndex << 2) + 3);
             }
             // delete the current region's indices
             currentRectangles.clear();
         }
 
         // insert to next level regions, until one of those insertions succeeds
-        insert(rectangleIndex, level + 1, 4 * regionIndex);
-        insert(rectangleIndex, level + 1, 4 * regionIndex + 1);
-        insert(rectangleIndex, level + 1, 4 * regionIndex + 2);
-        insert(rectangleIndex, level + 1, 4 * regionIndex + 3);
+        insert(rectangleIndex, level + 1, (regionIndex << 2));
+        insert(rectangleIndex, level + 1, (regionIndex << 2) + 1);
+        insert(rectangleIndex, level + 1, (regionIndex << 2) + 2);
+        insert(rectangleIndex, level + 1, (regionIndex << 2) + 3);
 
-        // LOG("Something went wrong in insert");
-        // exit(1);
+        return (false);
+    }
+};
+
+class Grid
+{
+    const uint8_t _gridLimit;
+    array<vector<uint32_t>, UINT16_MAX> _grids;
+    Rectangle _bound;
+    vector<Rectangle> _rectangles;
+public:
+    Grid(const Rectangle& bound)
+        : _gridLimit(10),
+        _bound(bound)
+    {}
+
+    void insert(const Rectangle& rectangle)
+    {
+        if (_bound.doesIntersect(rectangle) == false)
+            return ;
+        _rectangles.push_back(rectangle);
+        insert(_rectangles.size() - 1);
+    }
+
+    uint32_t checkIntersections(void)
+    {
+        uint32_t nOfIntersections = 0;
+        for (const auto& grid : _grids)
+        {
+            for (uint32_t i = 0; i < grid.size(); ++i)
+            {
+                for (uint32_t j = i + 1; j < grid.size(); ++j)
+                {
+                    if (_rectangles[grid[i]].doesIntersect(_rectangles[grid[j]]))
+                    {
+                        ++nOfIntersections;
+                    }
+                }
+            }
+        }
+
+        return (nOfIntersections);
+    }
+private:
+    bool insert(uint32_t rectangleIndex)
+    {
+        float cellWidth = (float)_bound.width / (UINT8_MAX + 1);
+        float cellHeight = (float)_bound.height / (UINT8_MAX + 1);
+        Rectangle rectangle = _rectangles[rectangleIndex];
+        // Bounds checking
+        if (rectangle.topLeftX < _bound.topLeftX)
+        {
+            rectangle.topLeftX = _bound.topLeftX;
+        }
+        if (rectangle.topLeftY < _bound.topLeftY)
+        {
+            rectangle.topLeftY = _bound.topLeftY;
+        }
+        if (rectangle.topLeftX + rectangle.width > _bound.topLeftX + _bound.width)
+        {
+            rectangle.width = _bound.topLeftX + _bound.width - rectangle.topLeftX - 1;
+        }
+        if (rectangle.topLeftY + rectangle.height > _bound.topLeftY + _bound.height)
+        {
+            rectangle.height = _bound.topLeftY + _bound.height - rectangle.topLeftY - 1;
+        }
+        uint16_t topLeft = ((uint16_t)((rectangle.topLeftY -_bound.topLeftY) / cellHeight) << 8)
+            + (uint16_t)((rectangle.topLeftX -_bound.topLeftX) / cellWidth);
+        uint16_t topRight = ((uint16_t)((rectangle.topLeftY -_bound.topLeftY) / cellHeight) << 8)
+            + (uint16_t)((rectangle.topLeftX + rectangle.width -_bound.topLeftX) / cellWidth);
+        uint16_t botLeft = ((uint16_t)((rectangle.topLeftY + rectangle.height -_bound.topLeftY) / cellHeight) << 8)
+            + (uint16_t)((rectangle.topLeftX -_bound.topLeftX) / cellWidth);
+        uint16_t botRight = ((uint16_t)((rectangle.topLeftY + rectangle.height -_bound.topLeftY) / cellHeight) << 8)
+            + (uint16_t)((rectangle.topLeftX + rectangle.width -_bound.topLeftX) / cellWidth);
+
+        // LOG(topLeft);
+        // LOG(topRight);
+        // LOG(botLeft);
+        // LOG(botRight);
+
+        // bounds are not correct, botleft - topleft is 256 instead of 0 in this test case, it should be 1 at most
+        for (uint16_t y = 0; y <= botLeft - topLeft; ++y)
+        {
+            for (uint16_t x = topLeft; x <= topRight; ++x)
+            {
+                _grids[x + y].push_back(rectangleIndex);
+            }
+        }
+
         return (false);
     }
 };
 
 float myRand(float low, float high)
 {
-    return (low + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(high - low))));
+    return (low + static_cast <float> (rand()) / static_cast <float>(RAND_MAX) * (high - low));
+}
+
+int myRand(int low, int high)
+{
+    return (rand() % (high - low + 1) + low);
 }
 
 int main()
 {
+    pow2(0);
+    pow4(0);
     srand(42);
-    float low = 0.0f;
-    float high = 10000.0f;
-    unsigned int numberOfInsertions = 100;
+    int low = 0;
+    int high = 100000;
+    unsigned int numberOfInsertions = 1000;
     ImprovedQuadTree qt({low, low, high, high});
     QuadTree qt1({low, low, high, high});
+    Grid grid({low, low, high, high});
 
     vector<Rectangle> rectangles;
     for (unsigned int index = 0; index < numberOfInsertions; ++index)
     {
-        rectangles.push_back({myRand(low, high), myRand(low, high), myRand(low, high) / 10.0f, myRand(low, high) / 10.0f});
+        rectangles.push_back({myRand(low, high), myRand(low, high), 50, 50});
     }
 
     LOG("Improved QuadTree");
@@ -317,5 +476,22 @@ int main()
     LOG("Checking intersections between the " << numberOfInsertions << " rectangles");
     start = clock();
     LOG("Number of intersections: " << qt1.checkIntersections());
+    LOG("Finished checking for intersections, time taken: " << (clock() - start) / (float)(CLOCKS_PER_SEC) << "s");
+
+
+    LOG("");
+    LOG("Grid");
+    LOG("Making grid with " << numberOfInsertions << " elements");
+    start = clock();
+    for (const auto& rectangle : rectangles)
+    {
+        // LOG(rectangle);
+        grid.insert(rectangle);
+    }
+    LOG("Finished making grid, time taken: " << (clock() - start) / (float)(CLOCKS_PER_SEC) << "s");
+
+    LOG("Checking intersections between the " << numberOfInsertions << " rectangles");
+    start = clock();
+    LOG("Number of intersections: " << grid.checkIntersections());
     LOG("Finished checking for intersections, time taken: " << (clock() - start) / (float)(CLOCKS_PER_SEC) << "s");
 }
