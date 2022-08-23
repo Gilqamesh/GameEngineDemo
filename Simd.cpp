5a4d6de183d364b03d9cf06b4363b8dd8d8ad6ee
Érchegyi Dávid <celicaw8@gmail.com>
2022-08-23 12:24:52 +0200

    add Simd.cpp

--- Simd.cpp
+++ Simd.cpp
@@ -66,4 +66,14 @@ int main()
     if (_mm256_movemask_pd(_mm256_castsi256_pd(_mask2)) > 0)
         goto repeat;
     LOG("Sup");
+
+// unpack vector into 4 ints
+#if defined (__WIN32)
+    int a[4];
+    a[0] = int(_n.m256i_i64[3]);
+    a[1] = int(_n.m256i_i64[2]);
+    a[2] = int(_n.m256i_i64[1]);
+    a[3] = int(_n.m256i_i64[0]);
+#endif
+
 }
